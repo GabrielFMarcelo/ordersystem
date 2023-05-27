a7297a03ce256b46bab7d44a163ebb1de13022c3
GabrielFMarcelo <124745233+GabrielFMarcelo@users.noreply.github.com>
2023-05-27 15:51:00 +0800

    Update cart.php

--- cart.php
+++ cart.php
@@ -40,15 +40,6 @@ require_once 'resource/php/class/config.php';
       </nav>
     </header>
 
-    <?php
-   echo "<br>";
-   $view = new view();
-   $view->userInfo();
-   ?>
-
-    <?php
-    insertN();
-     ?>
 
      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
